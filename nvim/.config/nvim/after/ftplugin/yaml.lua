af1fd396ebf89c6d83d69601c8020280a35a5b46
kmolski <krzysztof.molski29@gmail.com>
2022-07-28 16:02:01 +0200

    Update neovim config to use Lua

---
+++ nvim/.config/nvim/after/ftplugin/yaml.lua
@@ -0,0 +1,4 @@
+-- Indent settings for YAML files
+vim.opt.shiftwidth = 2   -- Indents use 2 spaces
+vim.opt.tabstop = 2      -- Tabs are displayed as 2 spaces
+vim.opt.expandtab = true -- Insert 2 spaces instead of tabs
---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,23 @@
+-- Include plugins
+require('plugins')
+
+-- Line numbers on the side
+vim.opt.number = true
+
+-- Four space indents for most files
+vim.opt.shiftwidth = 4   -- Indents use 4 spaces
+vim.opt.tabstop = 4      -- Tabs are displayed as 4 spaces
+vim.opt.expandtab = true -- Insert 4 spaces instead of tabs
+
+-- Enable text selection with mouse
+vim.opt.mouse = "a"
+
+-- Enable true color support in terminal
+vim.opt.termguicolors = true
+
+-- Incremental search and replace
+vim.opt.incsearch = true
+vim.opt.inccommand = "nosplit"
+
+-- Global replace by default
+vim.opt.gdefault = true
