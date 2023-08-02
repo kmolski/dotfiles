23496f1422ee435871d26f819585a63c265897d0
kmolski <krzysztof.molski29@gmail.com>
2023-08-02 12:47:31 +0200

    Reformat Lua files

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,22 +1,16 @@
 return require("packer").startup(function(use)
     -- Packer can manage itself
     use "wbthomason/packer.nvim"
-
     -- Tree sitter support
     use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
-
     -- Git integration
     use "tpope/vim-fugitive"
-
     -- Fuzzy finder
     use { "nvim-telescope/telescope.nvim", branch = '0.1.x', requires = {{ "nvim-lua/plenary.nvim" }} }
-
     -- Undo tree
     use "mbbill/undotree"
-
     -- Color schemes
     use "ellisonleao/gruvbox.nvim"
-
     -- LSP support
     use {
         "VonHeikemen/lsp-zero.nvim",
@@ -25,7 +19,6 @@ return require("packer").startup(function(use)
             {"neovim/nvim-lspconfig"},
             {"williamboman/mason.nvim"},
             {"williamboman/mason-lspconfig.nvim"},
-
             -- Autocompletion
             {"hrsh7th/nvim-cmp"},
             {"hrsh7th/cmp-buffer"},
@@ -33,7 +26,6 @@ return require("packer").startup(function(use)
             {"saadparwaiz1/cmp_luasnip"},
             {"hrsh7th/cmp-nvim-lsp"},
             {"hrsh7th/cmp-nvim-lua"},
-
             -- Snippets
             {"L3MON4D3/LuaSnip"},
             -- Snippet collection
--- nvim/.config/nvim/lua/treesitter.lua
+++ nvim/.config/nvim/lua/treesitter.lua
@@ -1,13 +1,9 @@
 require("nvim-treesitter.configs").setup({
     ensure_installed = { "c", "java", "javascript", "lua", "python", "rust", "typescript", "help" },
-
     sync_install = false,
     auto_install = true,
-
     highlight = {
-        -- `false` will disable the whole extension
         enable = true,
-
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
