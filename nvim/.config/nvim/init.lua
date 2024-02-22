f8af0ba2e7c7b711c7f3b18d72174724c304e885
kmolski <krzysztof.molski29@gmail.com>
2024-02-22 21:04:22 +0100

    Update Neovim init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,24 +4,24 @@ require("lsp")
 require("remap")
 require("treesitter")
 
--- Relative line numbers on the side
+-- Relative line numbers on the side, no line wrap
 vim.opt.number         = true
 vim.opt.relativenumber = true
+vim.opt.wrap           = false
 
 -- Four space indents for most files
 vim.opt.shiftwidth = 4    -- Indents use 4 spaces
 vim.opt.tabstop    = 4    -- Tabs are displayed as 4 spaces
 vim.opt.expandtab  = true -- Insert 4 spaces instead of tabs
 
--- Enable text selection with mouse
-vim.opt.mouse = "a"
-
--- Enable true color support in terminal
-vim.opt.termguicolors = true
-
--- Incremental search and replace
+-- Incremental search and replace, global by default
 vim.opt.incsearch  = true
 vim.opt.inccommand = "nosplit"
+vim.opt.gdefault   = true
 
--- Global replace by default
-vim.opt.gdefault = true
+-- Mouse selection and true color support in terminal
+vim.opt.mouse         = "a"
+vim.opt.termguicolors = true
+
+-- Highlight trailing whitespace
+vim.cmd.match("errorMsg", "/\\s\\+$/")
